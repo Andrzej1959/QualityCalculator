763e3aa16d1e8dbcb7a0b81764d4084b264c5573
luksza <luksza@am.gdynia.pl>
2019-02-05 23:13:39 +0100

    Dodanie liczenia średniej

--- app/src/androidTest/java/pl/edu/agh/qualitycalculator/MainActivityTest.java
+++ app/src/androidTest/java/pl/edu/agh/qualitycalculator/MainActivityTest.java
@@ -51,12 +51,64 @@ public class MainActivityTest {
         onView(withId(R.id.tvResult)).check(matches(withText("4.0 divided by 4.0 gives value 1.0")));
     }
     @Test
+    public void testDiv0(){
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText("4"));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText("0"));
+        onView(withId(R.id.btnDiv)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("4.0 divided by 0.0 gives value Infinity")));
+    }
+    @Test
+    public void testDiv00(){
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText("0"));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText("0"));
+        onView(withId(R.id.btnDiv)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("0.0 divided by 0.0 gives value NaN")));
+    }
+    @Test
+    public void testEmpty1(){
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText(""));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText("6"));
+        onView(withId(R.id.btnDiv)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("Enter some data to calculate")));
+    }
+    @Test
+    public void testEmpty12(){
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText("7"));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText(""));
+        onView(withId(R.id.btnDiv)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("Enter some data to calculate")));
+    }
+    @Test
     public void testAvg(){
         onView(withId(R.id.etNum1)).perform(click()).perform(typeText("3"));
         onView(withId(R.id.etNum2)).perform(click()).perform(typeText("4"));
         onView(withId(R.id.etNum3)).perform(click()).perform(typeText("5"));
         onView(withId(R.id.btnAvg)).perform(click());
         onView(withId(R.id.tvResult)).check(matches(withText("average of 3.0, 4.0, 5.0 gives value 4.0")));
+    }
+    @Test
+    public void testAvgEmpty1(){
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText(""));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText("4"));
+        onView(withId(R.id.etNum3)).perform(click()).perform(typeText("7"));
+        onView(withId(R.id.btnAvg)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("Enter some data to calculate")));
+    }
+    @Test
+    public void testAvgEmpty2(){
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText("4"));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText(""));
+        onView(withId(R.id.etNum3)).perform(click()).perform(typeText("7"));
+        onView(withId(R.id.btnAvg)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("Enter some data to calculate")));
+    }
+    @Test
+    public void testAvgEmpty3(){
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText("3"));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText("4"));
+        onView(withId(R.id.etNum3)).perform(click()).perform(typeText(""));
+        onView(withId(R.id.btnAvg)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("Enter some data to calculate")));
     }
 
 }
